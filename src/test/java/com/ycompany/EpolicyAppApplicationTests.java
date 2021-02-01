955346e1c0af3c7b86d027a5500b4a50e03da92b
samson1487 <sumit.samson87@outlook.com>
2021-02-01 16:03:10 +0530

    failing test added

--- src/test/java/com/ycompany/EpolicyAppApplicationTests.java
+++ src/test/java/com/ycompany/EpolicyAppApplicationTests.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Assert;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class EpolicyAppApplicationTests {
 
-	//@Test
-	public void contextLoads() {
+	@Test
+	public void test1() {
+		 Assert.assertTrue(false);
 	}
 
 	
